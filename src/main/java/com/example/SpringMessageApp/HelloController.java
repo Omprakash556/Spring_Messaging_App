5a79a2b17861f5d70a5cb81c4dcfafd0d36e0621
Om Prakash <ompandit416@gmail.com>
2025-02-23 09:39:01 +0530

    [Om] add. Create a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User. To begin with show Hello from BridgeLabz

---
+++ src/main/java/com/example/SpringMessageApp/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.SpringMessageApp;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+public class HelloController {
+    @RequestMapping("/uc1")
+    public String sayHello(){
+        return "Hello from BridgeLabz!";
+    }
+}
